3de39bfe04067fd0176cd0d90861bc1a56da5925
pjhartzell <preston.hartzell@gmail.com>
2022-02-07 14:01:09 -0500

    remove newlines from collection descriptions

--- src/stactools/nclimgrid/constants.py
+++ src/stactools/nclimgrid/constants.py
@@ -48,17 +48,17 @@ TRANSFORM = [
 
 DAILY_COLLECTION_ID = "nclimgrid-daily"
 DAILY_COLLECTION_TITLE = "NOAA Daily U.S. Climate Gridded Dataset (NClimGrid-d)"
-DAILY_COLLECTION_DESCRIPTION = """The NOAA Daily U.S. Climate Gridded Dataset
-(NClimGrid-d) consists of four climate variables derived from the Global
-Historical Climatology Network Daily dataset (GHCN-D): maximum temperature,
-minimum temperature, average temperature, and precipitation. Daily values in a
-1/24 degree lat/lon (nominal 5x5 kilometer) grid are provided for the
-Continental United States. Daily data is available from 1951 to the present.
-
-On an annual basis, approximately one year of “final” nClimGrid will be
-submitted to replace the initially supplied “preliminary” data for the same
-time period. Users should be sure to ascertain which level of data is required
-for their research."""
+DAILY_COLLECTION_DESCRIPTION = (
+    "The NOAA Daily U.S. Climate Gridded Dataset (NClimGrid-d) consists of "
+    "four climate variables derived from the Global Historical Climatology "
+    "Network Daily dataset (GHCN-D): maximum temperature, minimum temperature, "
+    "average temperature, and precipitation. Daily values in a 1/24 degree "
+    "lat/lon (nominal 5x5 kilometer) grid are provided for the Continental "
+    "United States. Daily data is available from 1951 to the present.\n\n"
+    "On an annual basis, approximately one year of “final” nClimGrid will be "
+    "submitted to replace the initially supplied “preliminary” data for the "
+    "same time period. Users should be sure to ascertain which level of data "
+    "is required for their research.")
 DAILY_COLLECTION_KEYWORDS = [
     "Air Temperature", "Precipitation", "Surface Observations",
     "Daily Climatology", "CONUS"
@@ -66,17 +66,18 @@ DAILY_COLLECTION_KEYWORDS = [
 
 MONTHLY_COLLECTION_ID = "nclimgrid-monthly"
 MONTHLY_COLLECTION_TITLE = "NOAA Monthly U.S. Climate Gridded Dataset (NClimGrid)"
-MONTHLY_COLLECTION_DESCRIPTION = """The NOAA Monthly U.S. Climate Gridded Dataset
-(NClimGrid) consists of four climate variables derived from the Global
-Historical Climatology Network Daily dataset (GHCN-D): maximum temperature,
-minimum temperature, average temperature, and precipitation. Monthly values in a
-in a 1/24 degree lat/lon (nominal 5x5 kilometer) grid are provided for the
-Continental United States. Monthly data is available from 1895 to the present.
+MONTHLY_COLLECTION_DESCRIPTION = (
+    "The NOAA Monthly U.S. Climate Gridded Dataset (NClimGrid) consists of "
+    "four climate variables derived from the Global Historical Climatology "
+    "Network Daily dataset (GHCN-D): maximum temperature, minimum temperature, "
+    "average temperature, and precipitation. Monthly values in a 1/24 degree "
+    "lat/lon (nominal 5x5 kilometer) grid are provided for the Continental "
+    "United States. Monthly data is available from 1895 to the present.\n\n"
+    "On an annual basis, approximately one year of “final” nClimGrid will be "
+    "submitted to replace the initially supplied “preliminary” data for the "
+    "same time period. Users should be sure to ascertain which level of data "
+    "is required for their research.")
 
-On an annual basis, approximately one year of “final” nClimGrid will be
-submitted to replace the initially supplied “preliminary” data for the same
-time period. Users should be sure to ascertain which level of data is required
-for their research."""
 MONTHLY_COLLECTION_KEYWORDS = [
     "Air Temperature", "Precipitation", "Surface Observations",
     "Monthly Climatology", "CONUS"
